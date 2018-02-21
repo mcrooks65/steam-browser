53df234e51e6c195242577e0d08348e4fac29b67
Matthew Crooks <mfcrooks@gmail.com>
2018-02-21 05:02:26 +0000

    Update cli.rb w/ menu method

--- lib/steam_browser/cli.rb
+++ lib/steam_browser/cli.rb
@@ -25,14 +25,23 @@ class SteamBrowser::CLI
   end
 
   def menu
-    # input = nil
-    # while input != "q"
-    #   puts "Enter the number of the game you'd like more info on or 'q' to quit."
-    #   input = gets.strip.downcase
-    #   case input
-    #   when "1"
-    #     puts "FreeHolder - $9.99 (SALE $5.99) - Strategy/RPG/Survival"
-    #   when "2"
+    input = nil
+    while input != "q"
+      puts "Enter the number of the game you'd like more info on or 'q' to quit."
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "FreeHolder\n$9.99 (SALE $5.99)\nPositive (83%\n31 Reviews)\nStrategy/RPG/Indie"
+      when "2"
+        puts "Kerbal Space Program\n$39.99 (Not on Sale)\nOverwhelmingly Positive (95%\n37,887 Reviews)\nSpace/Simulation/Sandbox"
+      when "3"
+        puts "The Long Dark\n$34.99 (Not on Sale)\nVery Positive (92%\n32,524 Reviews)\nSurvival/Open World/Atmospheric"
+      when "4"
+        puts "Rimworld \n$29.99 (Not on Sale) \nOverwhelmingly Positive (97%\n17,104 Reviews) \nEarly Access/Base Building/Survival"
+      when "5"
+        puts "Doom\n$29.99 (Not on Sale)\nVery Positive (92%\n33,579 Reviews)\nFPS/Action/Gore"
+      end
+    end
   end
 
 end
