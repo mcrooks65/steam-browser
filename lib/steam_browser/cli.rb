6fd765ea8cb26376b969cb06695f0bb4357be83c
Matthew Crooks <mfcrooks@gmail.com>
2018-02-21 04:44:03 +0000

    Update cli.rb w/ example game list

--- lib/steam_browser/cli.rb
+++ lib/steam_browser/cli.rb
@@ -1,8 +1,38 @@
 #Our CLI Controller
+
+
 class SteamBrowser::CLI
 
   def call
-    puts "Today's Games:"
+    welcome
+    matts_picks
+    menu
+  end
+
+  def welcome
+    puts "Welcome to Steam Browser v0.0.1"
+  end
+
+  def matts_picks
+    puts <<-DOC.gsub /^\s*/, ''
+      Here's Matt's top game picks.
+      1. FreeHolder
+      2. Kerbal Space Program
+      3. The Long Dark
+      4. Rimworld
+      5. Doom
+    DOC
+  end
+
+  def menu
+    # input = nil
+    # while input != "q"
+    #   puts "Enter the number of the game you'd like more info on or 'q' to quit."
+    #   input = gets.strip.downcase
+    #   case input
+    #   when "1"
+    #     puts "FreeHolder - $9.99 (SALE $5.99) - Strategy/RPG/Survival"
+    #   when "2"
   end
 
 end
