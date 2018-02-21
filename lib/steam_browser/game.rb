e69838c69b3a03d7138da0409aa83a8527eccc9e
Matthew Crooks <mfcrooks@gmail.com>
2018-02-21 06:19:38 +0000

    Stub out game.rb with fake data

--- lib/steam_browser/game.rb
+++ lib/steam_browser/game.rb
@@ -16,13 +16,37 @@ class SteamBrowser::Game
     game_1.title = "FreeHolder"
     game_1.price = "$9.99"
     game_1.sale_price = "$5.99"
-    game_1.rating = "(83% - 31 Reviews)"
+    game_1.rating = "Positive (83% - 31 Reviews)"
     game_1.genres = "Strategy/RPG/Indie"
 
-  # "Kerbal Space Program\n$39.99 (Not on Sale)\nOverwhelmingly Positive (95% - 37,887 Reviews)\nSpace/Simulation/Sandbox"
-  # "The Long Dark\n$34.99 (Not on Sale)\nVery Positive (92% - 32,524 Reviews)\nSurvival/Open World/Atmospheric"
-  # "Rimworld \n$29.99 (Not on Sale) \nOverwhelmingly Positive (97% - 17,104 Reviews) \nEarly Access/Base Building/Survival"
-  # "Doom\n$29.99 (Not on Sale)\nVery Positive (92% - 33,579 Reviews)\nFPS/Action/Gore"
+    game_2 = self.new
+    game_2.title = "Kerbal Space Program"
+    game_2.price = "$39.99"
+    game_2.sale_price = nil
+    game_2.rating = "Overwhelmingly Positive (95% - 37,887 Reviews)"
+    game_2.genres = "Space/Simulation/Sandbox"
 
+    game_3 = self.new
+    game_3.title = "The Long Dark"
+    game_3.price = "$34.99"
+    game_3.sale_price = nil
+    game_3.rating = "Very Positive (92% - 32,524 Reviews)"
+    game_3.genres = "Survival/Open World/Atmospheric"
+
+    game_4 = self.new
+    game_4.title = "Rimworld"
+    game_4.price = "$29.99"
+    game_4.sale_price = nil
+    game_4.rating = "Overwhelmingly Positive (97% - 17,104 Reviews)"
+    game_4.genres = "Early Access/Base Building/Survival"
+
+    game_5 = self.new
+    game_5.title = "Doom"
+    game_5.price = "$29.99"
+    game_5.sale_price = nil
+    game_5.rating = "Very Positive (92% - 33,579 Reviews)"
+    game_5.genres = "FPS/Action/Gore"
+
+    [game_1, game_2, game_3, game_4, game_5]
   end
 end
