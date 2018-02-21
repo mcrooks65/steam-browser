433686fa2f1bf76878d6d5e1267728366677a7ce
Matthew Crooks <mfcrooks@gmail.com>
2018-02-21 23:03:08 +0000

    Finish CLI update w/ Game object access

--- lib/steam_browser/cli.rb
+++ lib/steam_browser/cli.rb
@@ -26,15 +26,17 @@ class SteamBrowser::CLI
       puts "Enter the number of the game you'd like more info on or 'q' to quit."
       input = gets.strip.downcase
 
-      if input.to_i > 0
+      if input.to_i > 0 && input.to_i <= @games.length
         game_choice = @games[input.to_i-1]
-        puts game_choice
+        puts game_choice.title
+        puts game_choice.price
+        puts game_choice.rating
+        puts game_choice.genres
       elsif input == "list"
         matts_picks
       else
         puts "Not a valid input.  Type 'list' or 'q' to quit." unless input == 'q'
       end
-
     end
   end
 
@@ -43,13 +45,3 @@ class SteamBrowser::CLI
   end
 
 end
-#   when "1"
-#     puts "FreeHolder\n$9.99 (SALE $5.99)\nPositive (83% - 31 Reviews)\nStrategy/RPG/Indie"
-#   when "2"
-#     puts "Kerbal Space Program\n$39.99 (Not on Sale)\nOverwhelmingly Positive (95% - 37,887 Reviews)\nSpace/Simulation/Sandbox"
-#   when "3"
-#     puts "The Long Dark\n$34.99 (Not on Sale)\nVery Positive (92% - 32,524 Reviews)\nSurvival/Open World/Atmospheric"
-#   when "4"
-#     puts "Rimworld \n$29.99 (Not on Sale) \nOverwhelmingly Positive (97% - 17,104 Reviews) \nEarly Access/Base Building/Survival"
-#   when "5"
-#     puts "Doom\n$29.99 (Not on Sale)\nVery Positive (92% - 33,579 Reviews)\nFPS/Action/Gore"
