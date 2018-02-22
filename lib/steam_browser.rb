1c5032dab66ab06331f16c0a82fb250c71aebaef
Matthew Crooks <mfcrooks@gmail.com>
2018-02-22 00:13:45 +0000

    Update scraper environment dependencies

--- lib/steam_browser.rb
+++ lib/steam_browser.rb
@@ -1,4 +1,8 @@
 # Environment File
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
 require_relative './steam_browser/version'
 require_relative './steam_browser/cli'
 require_relative './steam_browser/game'
