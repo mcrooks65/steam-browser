fd6e6badc67286a6b0e0596b50eecfe76714745f
Matthew Crooks <mfcrooks@gmail.com>
2018-02-22 03:05:32 +0000

    Update scrape_by_title w/ target_url

--- lib/steam_browser/game.rb
+++ lib/steam_browser/game.rb
@@ -1,65 +1,39 @@
 class SteamBrowser::Game
 
-  attr_accessor :title, :price, :sale_price, :rating, :genres
+  attr_accessor :title, :price, :sale_price, :rating, :genres  #Add :desc later for game descriptions.  Also consider adding :developer and :publisher as well.
 
-  MATTS_PICKS = ["FreeHolder", "Kerbal Space Program", "The Long Dark", "Rimworld", "Doom"]
+  MATTS_PICKS = ["FreeHolder", "Kerbal Space Program", "The Long Dark", "Rimworld", "Doom"] # NEED TO FIX SO DOOM CAN BE READ (AGE VERIFICAITON PAGE BLOCKING SCRAPER)
 
-  def self.matts_picks   # Matt's picks should have a hardcoded array of 5 game titles and then calls the scraper to scrape the appropriate Steam webpages to fill out game data.
+  def self.matts_picks   # Matt's picks should take a hardcoded constant array of 5 game titles and then calls the scraper to scrape the appropriate Steam webpages to fill out game data.
 
-    game_1 = self.scrape_by_title(MATTS_PICKS[0]) # This line should do what the 6 lines below do...
+    game_1 = self.scrape_by_title(MATTS_PICKS[0])
 
-    # game_1 = self.new
-    # game_1.title = "FreeHolder"
-    # game_1.price = "$9.99"
-    # game_1.sale_price = "$5.99"
-    # game_1.rating = "Positive (83% - 31 Reviews)"
-    # game_1.genres = "Strategy/RPG/Indie"
+    game_2 = self.scrape_by_title(MATTS_PICKS[1])
 
-    self.scrape_by_title(MATTS_PICKS[1])
+    game_3 = self.scrape_by_title(MATTS_PICKS[2])
 
-    game_2 = self.new
-    game_2.title = "Kerbal Space Program"
-    game_2.price = "$39.99"
-    game_2.sale_price = nil
-    game_2.rating = "Overwhelmingly Positive (95% - 37,887 Reviews)"
-    game_2.genres = "Space/Simulation/Sandbox"
+    game_4 = self.scrape_by_title(MATTS_PICKS[3])
 
-    self.scrape_by_title(MATTS_PICKS[2])
+#    game_5 = self.scrape_by_title(MATTS_PICKS[4]) # Scraping the Doom store page is currently being blocked by AGE VERIFICATION. This is a tough fix...
 
-    game_3 = self.new
-    game_3.title = "The Long Dark"
-    game_3.price = "$34.99"
-    game_3.sale_price = nil
-    game_3.rating = "Very Positive (92% - 32,524 Reviews)"
-    game_3.genres = "Survival/Open World/Atmospheric"
-
-    self.scrape_by_title(MATTS_PICKS[3])
-
-    game_4 = self.new
-    game_4.title = "Rimworld"
-    game_4.price = "$29.99"
-    game_4.sale_price = nil
-    game_4.rating = "Overwhelmingly Positive (97% - 17,104 Reviews)"
-    game_4.genres = "Early Access/Base Building/Survival"
-
-    self.scrape_by_title(MATTS_PICKS[4])
-
-    game_5 = self.new
-    game_5.title = "Doom"
-    game_5.price = "$29.99"
-    game_5.sale_price = nil
-    game_5.rating = "Very Positive (92% - 33,579 Reviews)"
-    game_5.genres = "FPS/Action/Gore"
-
-    [game_1, game_2, game_3, game_4, game_5]
+    [game_1, game_2, game_3, game_4] #game_5
   end
 
   def self.scrape_by_title(title) # Class method should take one argument, a game title string, and scrape the appropriate steam store page for data and then return that Game object.
     game = self.new
 
-    # Scraper goes here!  Fill game up with title, price, rating, genres!
-    doc = Nokogiri::HTML(open("http://store.steampowered.com/search/?term=#{title}")) # First Search
-    binding.pry
+    search_doc = Nokogiri::HTML(open("http://store.steampowered.com/search/?term=#{title}")) # First Search
+    target_url = search_doc.search("#search_result_container > div:nth-child(2) > a:nth-child(2)").first.attr("href").strip # Search target url acquisition
+
+    #Next Step - Drill down for game details with new target_url
+    doc = Nokogiri::HTML(open(target_url)) # Load doc with new search target url
+
+    game.title = doc.search("div.apphub_AppName").text
+    game.price = doc.search("div.game_purchase_price.price").text.strip # Needs fixing - Sometimes gives extra prices
+    game.rating = doc.search("div.user_reviews_summary_row").first.attr("data-store-tooltip").strip
+    game.genres = doc.search("div.glance_tags.popular_tags").text.strip.gsub("\t", "").gsub("\r", "").gsub("\n", "/").gsub("+", "")
+
+    #binding.pry
     game
   end
 end
